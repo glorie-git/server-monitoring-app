64281c8fb09ea37dc1bcc07544f6ce6d5d380b64
Glorie Ramazani <glorieramazani@gmail.com>
2022-06-04 13:33:28 -0700

    Creates ServerService.java interface.

--- src/main/java/io/getarrays/server/service/ServerService.java
+++ src/main/java/io/getarrays/server/service/ServerService.java
@@ -2,10 +2,13 @@ package io.getarrays.server.service;
 
 import io.getarrays.server.model.Server;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
+    Server ping(String ipAddress) throws UnknownHostException, IOException;
     Collection<Server> list(int limit);
     Server get(Long id);
     Server update(Server server);
