37a8fd61f19aa7ca499d1634b687cc2edd39a6da
Glorie Ramazani <glorieramazani@gmail.com>
2022-06-04 11:50:50 -0700

    Initial commit

---
+++ src/main/java/io/getarrays/server/model/Server.java
@@ -0,0 +1,32 @@
+package io.getarrays.server.model;
+
+import io.getarrays.server.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Server {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    @Column(unique = true) // make sure ip address is unique
+    @NotEmpty(message = "IP address cannot be empty or null") // makes sure that the ip address is never empty
+    private String ipAddress;
+    private String name;
+    private String type;
+    private String memory;
+    private String imageUrl;
+    private Status status;
+}
