37fab2c64ae7a20cf7ce959be1d75e4d8c8efb02
Glorie Ramazani <glorieramazani@gmail.com>
2022-06-06 11:22:31 -0700

    Adds server service file ts strict set to false

---
+++ serverapp/src/app/service/server.service.spec.ts
@@ -0,0 +1,18 @@
+// Test file
+
+import { TestBed } from '@angular/core/testing';
+
+import { ServerService } from './server.service';
+
+describe('ServerService', () => {
+  let service: ServerService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ServerService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ serverapp/src/app/service/server.service.ts
@@ -0,0 +1,77 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError} from 'rxjs';
+import { tap, catchError} from 'rxjs/operators';
+import { Status } from 'src/enum/status.enum';
+import { CustomResponse } from 'src/interface/custom-response';
+import { Server } from 'src/interface/server';
+
+@Injectable({ providedIn: 'root'})
+export class ServerService {
+
+  private readonly apiUrl = 'any';
+
+  constructor( private http: HttpClient) { } // dependency injection
+
+  // Procedural approach
+  // getServers(): Observable<CustomResponse> {
+  //   return this.http.get<CustomResponse>(`http://localhost:8080/server/list`);
+  // }
+
+  servers$ = <Observable<CustomResponse>> // observable used to retrieve a list of all the servers
+  this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
+  .pipe (
+    tap(console.log),
+    catchError(this.handleError)
+  )
+
+  save$ = (server: Server) => <Observable<CustomResponse>> // observable used to retrieve a list of all the servers
+  this.http.post<CustomResponse>(`${this.apiUrl}/server/save`, server)
+  .pipe (
+    tap(console.log),
+    catchError(this.handleError)
+  )
+
+  ping$ = (ipAddress: string) => <Observable<CustomResponse>> // observable used to retrieve a list of all the servers
+  this.http.get<CustomResponse>(`${this.apiUrl}/server/ping/${ipAddress}`)
+  .pipe (
+    tap(console.log),
+    catchError(this.handleError)
+  )
+
+  filter$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>> // observable used to retrieve a list of all the servers
+    new Observable<CustomResponse>(
+      subscriber => {
+        console.log(response);
+        subscriber.next(
+          status == Status.ALL ? { ...response, message: `Servers filtered by ${status} statys`} :
+          {
+            ...response,
+            message: response.data.servers
+            .filter(server => server.status === status).length > 0 ? `Server filtered by 
+            ${status === Status.SERVER_UP ? 'SERVER_UP' 
+            : 'SERVER_DOWN'} status` : `No servers of ${status} found`,
+            data: {servers: response.data.servers
+            .filter(server => server.status === status) }
+          }
+        );
+        subscriber.complete();
+      }
+    )
+  .pipe (
+    tap(console.log),
+    catchError(this.handleError)
+  )
+
+  delete$ = (serverId: number) => <Observable<CustomResponse>> // observable used to retrieve a list of all the servers
+  this.http.delete<CustomResponse>(`${this.apiUrl}/server/delete/${serverId}`)
+  .pipe (
+    tap(console.log),
+    catchError(this.handleError)
+  )
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error);
+    return throwError (`An error occurred - Error code: ${error.status}`);
+  }
+}
