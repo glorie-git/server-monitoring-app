87572ebb52a42c4810f8b5dd38c276827ec24f13
Glorie Ramazani <glorieramazani@gmail.com>
2022-06-06 09:58:06 -0700

    Fixed image local host url

--- src/main/java/io/getarrays/server/ServerApplication.java
+++ src/main/java/io/getarrays/server/ServerApplication.java
@@ -1,7 +1,13 @@
 package io.getarrays.server;
 
+import io.getarrays.server.model.Server;
+import io.getarrays.server.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static io.getarrays.server.enumeration.Status.SERVER_UP;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +16,11 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo) {
+		return args -> {
+			serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC",
+					"http://localhost:8080/server/image/server1.png", SERVER_UP));
+		};
+	}
 }
