b415e6e698781013526847243b2fa809cb516dae
Glorie Ramazani <glorieramazani@gmail.com>
2022-06-09 19:42:33 -0700

    Adds ping server frontend functionality

--- serverapp/src/app/app.component.ts
+++ serverapp/src/app/app.component.ts
@@ -17,6 +17,7 @@ export class AppComponent implements OnInit {
   readonly DataState = DataState;
   readonly Status = Status;
   private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
   filterStatus$ = this.filterSubject.asObservable();
   // Google rjs observables
 
@@ -25,10 +26,29 @@ export class AppComponent implements OnInit {
     this.appState$ = this.seververService.servers$
     .pipe (
       map(response => {
+        this.dataSubject.next(response);
         return { dataState: DataState.LOADED_STATE, appData: response}
       }),
       startWith({ dataState: DataState.LOADING_STATE}),
       catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error })
+      })
+    );
+  }
+
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress); // Server with the ipAddress will have the spinner instead of the router
+    this.appState$ = this.seververService.ping$(ipAddress) // makes a call to the backend
+    .pipe (
+      map(response => {
+        const index =  this.dataSubject.value.data.servers.findIndex(server => server.id === response.data.server.id)
+          this.dataSubject.value.data.servers[index] = response.data.server;
+        this.filterSubject.next(''); // sets value back to string so user can ping the server once more
+        return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value}
+      }),
+      startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value}),
+      catchError((error: string) => {
+        this.filterSubject.next('');
         return of({ dataState: DataState.ERROR_STATE, error })
       })
     );
